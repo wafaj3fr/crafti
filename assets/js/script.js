466d84433da708fcf136eb847a6f816f7e2852c3
wafa <wafaagaafar@gmail.com>
2024-01-04 06:29:08 +0400

    all files

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const addEventOnElements = function (elements, eventType, callback) {
+    for (let i = 0, len = elements.length; i < len; i++) {
+      elements[i].addEventListener(eventType, callback);
+    }
+  }
+  
+  
+  
+  const navbar = document.querySelector("[data-navbar]");
+  const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+  const overlay = document.querySelector("[data-overlay]");
+  
+  const toggleNav = function () {
+    navbar.classList.toggle("active");
+    overlay.classList.toggle("active");
+  }
+  
+  addEventOnElements(navTogglers, "click", toggleNav);
+  
+  const header = document.querySelector("[header]");
+
+  const headerActive = function () {
+    if (window.scrollY > 100) {
+        header.classList.add("active");
+    } else {
+        header.classList.remove("active");
+    }
+  }
+
+  window.addEventListener( "scroll", headerActive);
